46f766298a5d77f54966eb54c787e4ad000d48b3
shujack001 <shujack001@gmail.com>
2024-11-23 12:45:59 +0800

    ss

---
+++ inheritance2/add.java
@@ -0,0 +1,5 @@
+public class add {
+    public int adds(int x, int y){
+        return x + y;
+    }
+}
---
+++ inheritance2/extend.java
@@ -0,0 +1,7 @@
+public class extend extends add{
+    public static void main(String[] args){
+        extend f = new extend();
+
+        System.out.println(f.adds(1, 2));
+    }
+}
